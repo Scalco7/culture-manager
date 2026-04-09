5294a80fff22414695c9f5ad8efcd40187ddaf30
Felipe Scalco <fscalco7@gmail.com>
2026-04-09 06:22:48 -0300

    init

---
+++ app/src/main/java/br/com/culture/manager/cultureManager/RegisterWeatherActivity.java
@@ -0,0 +1,14 @@
+package br.com.culture.manager.cultureManager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_weather);
+    }
+}
