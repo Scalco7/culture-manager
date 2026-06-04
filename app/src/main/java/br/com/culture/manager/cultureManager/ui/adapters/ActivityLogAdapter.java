d96b8b48898632d077969e83edbf6f3da7b101e1
Felipe Scalco <fscalco7@gmail.com>
2026-06-04 12:13:26 -0300

    feat: create component for activity log

---
+++ app/src/main/java/br/com/culture/manager/cultureManager/ui/adapters/ActivityLogAdapter.java
@@ -0,0 +1,74 @@
+package br.com.culture.manager.cultureManager.ui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import br.com.culture.manager.cultureManager.R;
+import br.com.culture.manager.cultureManager.ui.models.ActivityLogModel;
+
+public class ActivityLogAdapter extends BaseAdapter {
+    private final Context context;
+    private final ArrayList<ActivityLogModel> logEntities;
+
+    private static class ActivityLogHolder {
+        public TextView textViewName;
+        public TextView textViewPlot;
+        public TextView textViewTimeSpent;
+        public TextView textViewDate;
+    }
+
+    public ActivityLogAdapter(Context context, ArrayList<ActivityLogModel> logs){
+        this.context = context;
+        this.logEntities = logs;
+    }
+
+    @Override
+    public int getCount() {
+        return logEntities.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return logEntities.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return logEntities.get(i).hashCode();
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        ActivityLogAdapter.ActivityLogHolder holder;
+
+        if(view == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.activity_log_component, parent, false);
+
+            holder = new ActivityLogAdapter.ActivityLogHolder();
+            holder.textViewName = view.findViewById(R.id.textViewActivityLogName);
+            holder.textViewPlot = view.findViewById(R.id.textViewActivityLogPlot);
+            holder.textViewTimeSpent = view.findViewById(R.id.textViewActivityLogTimeSpent);
+            holder.textViewDate = view.findViewById(R.id.textViewActivityLogDate);
+
+            view.setTag(holder);
+        }else{
+            holder = (ActivityLogAdapter.ActivityLogHolder) view.getTag();
+        }
+
+        ActivityLogModel activityLog = logEntities.get(position);
+
+        holder.textViewName.setText(activityLog.getName());
+        holder.textViewTimeSpent.setText(activityLog.getTimeSpent());
+        holder.textViewDate.setText(activityLog.getDate().toString());
+        holder.textViewPlot.setText(activityLog.getPlot().getName());
+
+        return view;
+    }
+}
---
+++ app/src/main/java/br/com/culture/manager/cultureManager/ui/models/ActivityLogModel.java
@@ -0,0 +1,61 @@
+package br.com.culture.manager.cultureManager.ui.models;
+
+import java.time.LocalDateTime;
+
+import br.com.culture.manager.cultureManager.domain.entities.PlotEntity;
+
+public class ActivityLogModel {
+    private long id;
+    private PlotEntity plot;
+    private String name;
+    private Integer timeSpent;
+    private LocalDateTime date;
+
+    public ActivityLogModel(LocalDateTime date, long id, String name, PlotEntity plot, Integer timeSpent) {
+        this.date = date;
+        this.id = id;
+        this.name = name;
+        this.plot = plot;
+        this.timeSpent = timeSpent;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Integer getTimeSpent() {
+        return timeSpent;
+    }
+
+    public void setTimeSpent(Integer timeSpent) {
+        this.timeSpent = timeSpent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PlotEntity getPlot() {
+        return plot;
+    }
+
+    public void setPlot(PlotEntity plot) {
+        this.plot = plot;
+    }
+}
